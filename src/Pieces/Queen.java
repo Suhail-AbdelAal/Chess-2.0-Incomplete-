940922513996aa7ff7058ca97d8387b3a085ce79
Suhail-Abdelall <souhail.abdelall01@gmail.com>
2021-09-16 15:11:45 +0200

    .

--- src/Pieces/Queen.java
+++ src/Pieces/Queen.java
@@ -28,29 +28,29 @@ public class Queen extends Piece{
         int y = this.getPosition().getyNum();
 
         // --- Linear Occupations ---
-        int lincer_occupations[] = super.Linear_Occupied_Spots(board, x, y);
+        int[] linear_occupations = super.Linear_Occupied_Spots(board, x, y);
 
         // Vertically
-        for (int i = lincer_occupations[0]; i <= lincer_occupations[1]; i++) {
+        for (int i = linear_occupations[0]; i <= linear_occupations[1]; i++) {
             if (i != x)
                 legalMoves.add(sq[i][y]);
         }
         // Horizontally
-        for (int i = lincer_occupations[3]; i <= lincer_occupations[2]; i++) {
+        for (int i = linear_occupations[3]; i <= linear_occupations[2]; i++) {
             if (i != y)
                 legalMoves.add(sq[x][i]);
         }
 
         // --- Diagonal Occupations ---
-        int diagonal_ocupations[] = super.Diagonal_Occupied_Spots(board, x, y);
+        int[] diagonal_occupations = super.Diagonal_Occupied_Spots(board, x, y);
 
         // TOP RIGHT -> BOTTOM LEFT
-        for (int i = diagonal_ocupations[0], j = diagonal_ocupations[1]; i <= diagonal_ocupations[6] && j >= diagonal_ocupations[7]; i++, j--) {
+        for (int i = diagonal_occupations[0], j = diagonal_occupations[1]; i <= diagonal_occupations[6] && j >= diagonal_occupations[7]; i++, j--) {
             if (i != x && j != y)
                 legalMoves.add(sq[i][j]);
         }
         // TOP LEFT -> BOTTOM RIGHT
-        for (int i = diagonal_ocupations[2], j = diagonal_ocupations[3]; i <= diagonal_ocupations[4] && j <= diagonal_ocupations[5]; i++, j++) {
+        for (int i = diagonal_occupations[2], j = diagonal_occupations[3]; i <= diagonal_occupations[4] && j <= diagonal_occupations[5]; i++, j++) {
             if (i != x && j != y)
                 legalMoves.add(sq[i][j]);
         }
